b433c1427d20b616e0ba23d6e37ad54a83a60616
Shafnat Fuaini Ramadhan <104710799+ShafnatFR@users.noreply.github.com>
2025-12-27 16:50:56 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,28 @@
-import { defineConfig } from 'vite';
+import path from 'path';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  // Setting base ke './' sangat PENTING untuk GitHub Pages
-  base: './',
-  server: {
-    // Ini mengizinkan akses dari Network (IP Address)
-    host: true
-  }
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, '.', '');
+    return {
+      // TAMBAHKAN BARIS INI:
+      // Ganti 'nama-repo-github-anda' dengan nama repository asli Anda di GitHub.
+      // Contoh: jika URL repo Anda github.com/shafnatfr/universe-expo-2025, maka isinya '/universe-expo-2025/'
+      base: '/universe-expo-2025/', 
+
+      server: {
+        port: 3000,
+        host: '0.0.0.0',
+      },
+      plugins: [react()],
+      define: {
+        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
+        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
+      },
+      resolve: {
+        alias: {
+          '@': path.resolve(__dirname, '.'),
+        }
+      }
+    };
 });
