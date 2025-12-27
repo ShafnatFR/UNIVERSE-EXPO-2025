4fa40797e11caaa4b54198ec91d341e4c14076ee
ShafnatFR <shafnatfuainiramadhan@gmail.com>
2025-12-27 16:38:42 +0700

    /

--- App.tsx
+++ App.tsx
@@ -6,32 +6,32 @@ import ProblemSection from './ProblemSection';
 import InspirationSection from './InspirationSection';
 import RSVPSection from './RSVPSection';
 import Footer from './Footer';
-import AlumniSection from './AlumniSection';
-import EventMap from './EventMap';
-import RegistrationForm from './RegistrationForm';
+// import AlumniSection from './AlumniSection';
+// import EventMap from './EventMap';
+// import RegistrationForm from './RegistrationForm';
 
 const App: React.FC = () => {
   return (
     <div className="relative min-h-screen text-slate-200 selection:bg-pastel-pink selection:text-black">
       <StarBackground />
       <Navbar />
-      
+
       <main>
         <section id="home">
           <Hero />
         </section>
-        
+
         <ProblemSection />
-        
+
         {/* Fitur Utama */}
-        <EventMap />
-        <AlumniSection />
-        
+        {/* <EventMap /> */}
+        {/* <AlumniSection /> */}
+
         <InspirationSection />
-        
+
         {/* ID Card Generator */}
-        <RegistrationForm />
-        
+        {/* <RegistrationForm /> */}
+
         {/* RSVP Section - Focused Content */}
         <div id="rsvp">
           <RSVPSection />
